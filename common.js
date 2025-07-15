94b54ad08d0805fa99e11f4a2ebdd34ee737c5ac
mrvnbrk1921 <marvin.birke@vfl-mettingen.de>
2025-07-15 19:10:27 +0200

    Update common.js

--- common.js
+++ common.js
@@ -1,13 +1,13 @@
 function fetchTrainingszeiten(targetMannschaft, sheetId, apiKey) {
   const urlTrainingszeiten = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Hallenplan?key=${apiKey}`;
-
+  
   fetch(urlTrainingszeiten)
     .then(response => response.json())
     .then(data => {
       const sheetData = data.values;
       const container = document.getElementById('trainingszeiten-data');
       const trainingszeiten = sheetData.filter(row =>
-        row[4] && row[4].trim() === targetMannschaft
+        row[0] && (row[4] === targetMannschaft || row[5] === targetMannschaft || row[6] === targetMannschaft || row[7] === targetMannschaft)
       );
 
       if (trainingszeiten.length === 0) {
@@ -17,12 +17,7 @@ function fetchTrainingszeiten(targetMannschaft, sheetId, apiKey) {
 
       let html = '<table class="trainingszeiten">';
       trainingszeiten.forEach(row => {
-        const ort = row[0];
-        const tag = row[1];
-        const start = row[2];
-        const ende = row[3];
-        const bezeichnung = row[5] ? `(${row[5]})` : '';
-
+        const tag = row[1], start = row[2], ende = row[3], halle = row[0], bezeichnung = row[8] ? `(${row[8]})` : '';
         html += `
           <tr>
             <td>${tag}.</td>
@@ -30,13 +25,12 @@ function fetchTrainingszeiten(targetMannschaft, sheetId, apiKey) {
             <td>-</td>
             <td>${ende}</td>
             <td>Uhr</td>
-            <td>${ort}</td>
+            <td>${halle}</td>
             <td>${bezeichnung}</td>
           </tr>
         `;
       });
       html += '</table>';
-      html += `<p><a target="_blank" href="https://vfl-mettingen-app.de/platzbelegung/#/">Änderungen und aktuelle Platzbelegung</a></p>`;
       container.innerHTML = html;
     })
     .catch(error => {
@@ -44,7 +38,6 @@ function fetchTrainingszeiten(targetMannschaft, sheetId, apiKey) {
       document.getElementById('trainingszeiten-data').innerHTML = `<p>Fehler: ${error.message}</p>`;
     });
 }
-
 
 function fetchTrainer(targetMannschaft, sheetId, apiKey) {
   const urlTrainer = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Trainer?key=${apiKey}`;
